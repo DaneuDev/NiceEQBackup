768d3dacccdcf9a3baa13799e9daa39a96708224
DrakeSun <kapruch007@gmail.com>
2023-05-18 23:22:18 +0200

    Changing from Set to Map saving users

--- src/main/java/pl/daneu/eqbackup/handlers/UserHandler.java
+++ src/main/java/pl/daneu/eqbackup/handlers/UserHandler.java
@@ -2,34 +2,24 @@ package pl.daneu.eqbackup.handlers;
 
 import pl.daneu.eqbackup.objects.User;
 
-import java.util.HashSet;
-import java.util.Set;
-import java.util.UUID;
+import java.util.*;
 
 public class UserHandler {
 
-    private static final Set<User> users = new HashSet<>();
+    private static final Map<UUID, User> users = new HashMap<>();
 
-    public static User getUser(UUID uuid){
-        return users.stream()
-                .filter(u -> u.getUUID().equals(uuid))
-                .findAny().orElse(null);
-    }
+    public static User getUser(UUID uuid){ return users.get(uuid); }
 
     public static User getUser(String playerName){
-        return users.stream()
+        return users.values().stream()
                 .filter(u -> u.getPlayerName().equalsIgnoreCase(playerName))
                 .findAny().orElse(null);
     }
 
-    public Set<User> getUsers(){ return users; }
+    public Map<UUID, User> getUsers(){ return users; }
 
-    public void addUser(User user){
-        users.add(user);
-    }
+    public void addUser(User user){ users.put(user.getUUID(), user); }
 
-    public void removeUser(UUID uuid){
-        users.removeIf(u -> u.getUUID().equals(uuid));
-    }
+    public void removeUser(UUID uuid){ users.remove(uuid); }
 
 }
